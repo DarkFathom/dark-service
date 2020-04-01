2399382bcd8e1bca4abeb0858932ab30881f9d60
DarkFathom <62442659+DarkFathom@users.noreply.github.com>
2020-03-31 21:29:36 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo 'Deploying to Jboss'
-$JBOSS_HOME/./jboss-cli.sh -c '--commands=deploy /build/*.war --force'
+#$JBOSS_HOME/./jboss-cli.sh -c '--commands=deploy /build/*.war --force'
+sudo /opt/jboss-eap/bin/./jboss-cli.sh -c '--commands=deploy /home/jenkins/workspace/name-service_master/build/dark-service.war --force'
