b6fcdc963fb2d8cd8d1cace17768b3ab9916f358
DarkFathom <62442659+DarkFathom@users.noreply.github.com>
2020-03-31 21:23:20 -0400

    Fix path to jboss home

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Deploying to Jboss'
-$JBOSS_HOME/bin/./jboss-cli.sh -c '--commands=deploy /build/*.war --force'
+$JBOSS_HOME/./jboss-cli.sh -c '--commands=deploy /build/*.war --force'
