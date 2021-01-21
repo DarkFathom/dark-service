d2de418360b673174e609932c5a0468abf6e4082
DarkFathom <62442659+DarkFathom@users.noreply.github.com>
2021-01-21 15:02:15 -0500

    Update service

--- src/main/java/dark/service/wildfly/HelloService.java
+++ src/main/java/dark/service/wildfly/HelloService.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1)
 public class HelloService extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         ArrayList<String> users = getNameString();
         response.setContentType("text/html");
