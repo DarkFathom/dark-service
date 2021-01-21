0052d0536576eba6f7a7405c34292676b9b2376e
DarkFathom <62442659+DarkFathom@users.noreply.github.com>
2021-01-21 14:59:19 -0500

    Update env vars and outputs

--- src/main/java/dark/service/wildfly/DBConnect.java
+++ src/main/java/dark/service/wildfly/DBConnect.java
@@ -5,9 +5,21 @@ import java.sql.*;
 
 public class DBConnect{
 
-  private String dbUrl = "jdbc:oracle:thin:@10.10.210.250:1521:ORCLCDB";
-  private String dbUser = "dummy";
-  private String dbPassword = "dummy";
+  //private String dbUrl = "jdbc:oracle:thin:@10.10.210.250:1521:ORCLCDB";
+  //private String dbUser = "dummy";
+  //private String dbPassword = "dummy";
+  private String dbDriver = "jdbc:oracle:thin:@";
+  private String dbPort_sid = ":1521:ORCLCDB";
+  private String env_dbHostVar = "ENV_DB_HOST_VARIABLE";
+  private String env_dbUser = "ENV_DB_USERNAME";
+  private String env_dbPassword = "ENV_DB_USERPASS";
+  private String dbHostVar = "";
+  private String dbHost = "";
+  private String dbUrl = "";
+  private String dbUser = "";
+  private String dbPassword = "";
+  private String dbConnection = "";
+
   private ArrayList<String> users = new ArrayList<String>();
 
   public ArrayList<String> getData(){
@@ -15,20 +27,44 @@ public class DBConnect{
   }
 
   public void setDataFromDB() {
+    /*
+      Read in value from configmap that contain db service host env variable
+      and use that to find the ip for the db.
+    */
+    try {
+        dbUser = System.getenv(env_dbUser);
+        dbPassword = System.getenv(env_dbPassword);
+        dbHostVar = System.getenv(env_dbHostVar);
+        dbHost = System.getenv(dbHostVar);
+        dbConnection = dbDriver + dbHost + dbPort_sid;
+    } catch  (Exception e) {
+        System.out.println("Error getting environment variables. " + e);
+    }
+
+    // Loggin into /wildfly/standalone/log/server.log
+    System.out.println("** Environment variables for db connection:");
+    System.out.format("%s=%s%n", env_dbHostVar, dbHostVar);
+    System.out.format("%s=%s%n", dbHostVar, dbHost);
+    System.out.format("%s=%s%n", "dbDriver", dbDriver);
+    System.out.format("%s=%s%n", "dbPort_sid", dbPort_sid);
+    System.out.format("%s=%s%n", env_dbUser, dbUser);
+    System.out.format("%s=%s%n", env_dbPassword, dbPassword);
+    System.out.println("** End of environment variables");
+    
     try (Connection conn = DriverManager.getConnection(
-        dbUrl, dbUser, dbPassword)) {
+        dbConnection, dbUser, dbPassword)) {
 
         if (conn != null) {
             System.out.println("Connected to the database!");
         
             Statement stmt=conn.createStatement();
-            ResultSet rs=stmt.executeQuery("select name from users");
-            
-            rs.last();
-            System.out.println("database query resulted in " + rs.getRow() + " records.");
-            
-            while(rs.next())
-              users.add(rs.getString(1));
+                        
+            ResultSet rsData=stmt.executeQuery("select name from users");
+
+            while(rsData.next()) {
+              users.add(rsData.getString(1));
+              System.out.println("Record: " + rsData.getString(1));
+            }
         } else {
             System.out.println("Failed to make connection!");
             users.add("connection error record");
